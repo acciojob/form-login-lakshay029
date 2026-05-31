e71c0e9e9e694ead98b9901342bceafd148afda6
lakshay <126890120+lakshay029@users.noreply.github.com>
2026-05-31 13:58:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
 function getFormvalue() {
-    //Write your code here
+            event.preventDefault();
 
-}
+            const fname = document.querySelector('input[name="fname"]').value.trim();
+            const lname = document.querySelector('input[name="lname"]').value.trim();
+
+            alert(`${fname} ${lname}`);
+        }
+
